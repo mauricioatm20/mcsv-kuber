d388855b931d0ddbad97ca0613d0373da00684ee
mauricio <mauriciochasi@gmail.com>
2025-08-21 10:36:08 +0200

    docker

--- msvc-cursos/src/main/java/org/chasi/springcloud/msvc/cursos/clients/UsuarioClientRest.java
+++ msvc-cursos/src/main/java/org/chasi/springcloud/msvc/cursos/clients/UsuarioClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-usuarios", url = "localhost:8081")
+@FeignClient(name = "msvc-usuarios", url = "msvc-usuarios:8081")
 public interface UsuarioClientRest {
 
     @GetMapping("/{id}")
